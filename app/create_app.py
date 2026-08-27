9c735e73a062dce89f3adedd8be973fd8f963d23
spline <dmcemin@gmail.com>
2026-08-28 00:22:42 +0200

    add: lifespan get taskiq_broker

--- app/create_app.py
+++ app/create_app.py
@@ -1,12 +1,26 @@
+from contextlib import asynccontextmanager
+from typing import TYPE_CHECKING
+
 from dishka import make_async_container
 from dishka.integrations.fastapi import setup_dishka as setup_fastapi_dishka
 from fastapi import FastAPI
+from taskiq_nats import PullBasedJetStreamBroker
 
 from app.dependencies.infrastructure import InfrastructureProvider
 
+if TYPE_CHECKING:
+    from collections.abc import AsyncIterator
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncIterator[None]:
+    await app.state.dishka_container.get(PullBasedJetStreamBroker)
+
+    yield
+
 
 def create() -> FastAPI:
-    app = FastAPI()
+    app = FastAPI(lifespan=lifespan)
 
     container = make_async_container(
         InfrastructureProvider(),
