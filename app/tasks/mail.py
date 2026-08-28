ef9aef1092608e91f97ae05b6606643ee7381365
spline <dmcemin@gmail.com>
2026-08-28 22:55:54 +0200

    use: TaskQueue

--- app/tasks/mail.py
+++ app/tasks/mail.py
@@ -6,10 +6,11 @@ from fastapi_mail import MessageType
 from pydantic import EmailStr
 from pydantic import NameEmail
 
+from app.constants.taskiq import TaskQueue
 from app.core.taskiq_broker import broker
 
 
-@broker.task(queue="mail")
+@broker.task(queue=TaskQueue.MAIL)
 @inject(patch_module=True)
 async def send_verification_email_task(
     fm: FromDishka[FastMail],
@@ -27,7 +28,7 @@ async def send_verification_email_task(
     await fm.send_message(message, template_name="mail/verify_email.html")
 
 
-@broker.task(queue="mail")
+@broker.task(queue=TaskQueue.MAIL)
 @inject(patch_module=True)
 async def send_reset_password_email_task(
     fm: FromDishka[FastMail],
