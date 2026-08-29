f2924c80cb34df49ad18c22fdad18d0c934be067
spline <dmcemin@gmail.com>
2026-08-29 23:04:49 +0200

    add: username limits

--- app/constants/user.py
+++ app/constants/user.py
@@ -1,3 +1,7 @@
 class UserLimits:
     PHOTO_URL_MIN: int = 11
     PHOTO_URL_MAX: int = 2048
+
+    USERNAME_MIN: int = 2
+    USERNAME_MAX: int = 20
+    ALLOWED_CHARS: frozenset[str] = frozenset("abcdefghijklmnopqrstuvwxyz0123456789_.")
