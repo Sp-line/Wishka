65030bb4289534a57b25941f43b40f0602f49c57
spline <dmcemin@gmail.com>
2026-08-29 21:42:55 +0200

    add: wishlist schemas

---
+++ app/schemas/wishlist.py
@@ -0,0 +1,61 @@
+from typing import Annotated
+
+from annotated_types import MaxLen
+from annotated_types import MinLen
+from pydantic import BaseModel
+from pydantic import ConfigDict
+from pydantic import Field
+from pydantic import PositiveInt
+
+from app.constants.wishlist import WishlistLimits
+from app.core.models.wishlist import generate_invite_token
+from app.schemas.base import Id
+
+type WishlistTitle = Annotated[
+    str,
+    MinLen(WishlistLimits.TITLE_MIN),
+    MaxLen(WishlistLimits.TITLE_MAX),
+]
+type WishlistDescription = Annotated[
+    str,
+    MinLen(WishlistLimits.DESCRIPTION_MIN),
+    MaxLen(WishlistLimits.DESCRIPTION_MAX),
+]
+type WishlistInviteToken = Annotated[
+    str,
+    MinLen(WishlistLimits.INVITE_TOKEN_MIN),
+    MaxLen(WishlistLimits.INVITE_TOKEN_MAX),
+]
+
+
+class WishlistBase(BaseModel):
+    title: WishlistTitle
+    description: WishlistDescription | None = None
+
+
+class WishlistCreateReq(WishlistBase): ...
+
+
+class WishlistCreateDB(WishlistBase):
+    owner_id: PositiveInt
+    invite_token: WishlistInviteToken = Field(default_factory=generate_invite_token)
+
+
+class WishlistUpdateReq(BaseModel):
+    title: WishlistTitle | None = None
+    description: WishlistDescription | None = None
+
+
+class WishlistUpdateDB(BaseModel):
+    title: WishlistTitle | None = None
+    description: WishlistDescription | None = None
+    invite_token: WishlistInviteToken | None = None
+
+    owner_id: PositiveInt | None = None
+
+
+class WishlistRead(Id, WishlistBase):
+    invite_token: WishlistInviteToken
+    owner_id: PositiveInt
+
+    model_config = ConfigDict(from_attributes=True)
