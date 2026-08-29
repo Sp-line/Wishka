9e58f78a4df5c9be7b356059a8a4ea1313b68ac6
spline <dmcemin@gmail.com>
2026-08-29 15:27:55 +0200

    add: get_aioboto3_session & get_s3_client provide

--- app/dependencies/infrastructure.py
+++ app/dependencies/infrastructure.py
@@ -1,12 +1,15 @@
 from collections.abc import AsyncIterable  # noqa: TC003
 from collections.abc import AsyncIterator  # noqa: TC003
+from typing import Any
 
+import aioboto3
 from dishka import Provider
 from dishka import Scope
 from dishka import provide
 from fastapi_mail import FastMail
 from sqlalchemy.ext.asyncio import AsyncSession  # noqa: TC002
 from taskiq_nats import PullBasedJetStreamBroker  # noqa: TC002
+from types_aiobotocore_s3 import S3Client  # noqa: TC002
 
 from app.core.config import settings
 from app.core.models.db import Database
@@ -51,3 +54,20 @@ class InfrastructureProvider(Provider):
     @provide(scope=Scope.APP)
     async def get_fast_mail(self) -> FastMail:
         return FastMail(settings.mail.conf)
+
+    @provide(scope=Scope.APP)
+    def get_aioboto3_session(self) -> aioboto3.Session:
+        return aioboto3.Session()
+
+    @provide(scope=Scope.APP)
+    async def get_s3_client(self, session: aioboto3.Session) -> AsyncIterator[S3Client]:
+        client_kwargs: dict[str, Any] = {
+            "service_name": "s3",
+            "region_name": settings.s3.region,
+            "endpoint_url": str(settings.s3.endpoint_url),
+            "aws_access_key_id": settings.s3.access_key.get_secret_value(),
+            "aws_secret_access_key": settings.s3.secret_key.get_secret_value(),
+        }
+
+        async with session.client(**client_kwargs) as client:
+            yield client
