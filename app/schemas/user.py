b681026f21debb2c3c6d0d09871d1c68cc0d82d9
spline <dmcemin@gmail.com>
2026-08-29 22:43:58 +0200

    add: photo_url for User schemas

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,8 +1,21 @@
+from typing import Annotated
+
 from fastapi_users.schemas import BaseUser
 from fastapi_users.schemas import BaseUserCreate
 from fastapi_users.schemas import BaseUserUpdate
+from pydantic import HttpUrl
+from pydantic import UrlConstraints
 
+from app.constants.user import UserLimits
 from app.core.types.user import UserID
+
+type UserPhotoUrl = Annotated[
+    HttpUrl,
+    UrlConstraints(
+        max_length=UserLimits.PHOTO_URL_MAX,
+        allowed_schemes=["http", "https"],
+    ),
+]
 
 
 class UserCreate(BaseUserCreate): ...
@@ -11,4 +24,5 @@ class UserCreate(BaseUserCreate): ...
 class UserUpdate(BaseUserUpdate): ...
 
 
-class UserRead(BaseUser[UserID]): ...
+class UserRead(BaseUser[UserID]):
+    photo_url: UserPhotoUrl | None = None
