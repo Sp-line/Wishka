bcd0faa37834dcf2b3cd21e014bc250422f10d6f
spline <dmcemin@gmail.com>
2026-09-01 23:36:13 +0200

    add: role for WishlistMember schemas

--- app/schemas/wishlist_member.py
+++ app/schemas/wishlist_member.py
@@ -2,10 +2,12 @@ from pydantic import BaseModel
 from pydantic import ConfigDict
 from pydantic import PositiveInt
 
+from app.constants.role import Role
 from app.schemas.base import Id
 
 
 class WishlistMemberBase(BaseModel):
+    role: Role = Role.PARTICIPANT
     wishlist_id: PositiveInt
     user_id: PositiveInt
 
@@ -16,6 +18,7 @@ class WishlistMemberCreateDB(WishlistMemberBase): ...
 class WishlistMemberUpdateDB(BaseModel):
     wishlist_id: PositiveInt | None = None
     user_id: PositiveInt | None = None
+    role: Role | None = None
 
 
 class WishlistMemberRead(Id, WishlistMemberBase):
