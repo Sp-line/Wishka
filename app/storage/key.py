22c91e6fb2de7fd87b36aac6112d5be6c823df53
spline <dmcemin@gmail.com>
2026-09-02 23:09:06 +0200

    add: UserAvatarKeyStrategy & GiftImageKeyStrategy

--- app/storage/key.py
+++ app/storage/key.py
@@ -1,5 +1,6 @@
 from abc import ABC
 from abc import abstractmethod
+from uuid import uuid4
 
 from app.schemas.storage import KeyBuild
 
@@ -10,3 +11,19 @@ class S3KeyStrategy[TKeyBuild: KeyBuild](ABC):
 
     @abstractmethod
     def generate(self, data: TKeyBuild) -> str: ...
+
+
+class UserAvatarKeyStrategy(S3KeyStrategy[KeyBuild]):
+    def __init__(self) -> None:
+        super().__init__(prefix="users/avatars")
+
+    def generate(self, _data: KeyBuild) -> str:
+        return f"{self._prefix}/{uuid4()}"
+
+
+class GiftImageKeyStrategy(S3KeyStrategy[KeyBuild]):
+    def __init__(self) -> None:
+        super().__init__(prefix="gifts/images")
+
+    def generate(self, _data: KeyBuild) -> str:
+        return f"{self._prefix}/{uuid4()}"
